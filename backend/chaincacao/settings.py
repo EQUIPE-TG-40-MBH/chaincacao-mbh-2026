b11af9b3b0d1450a876b8625545a449ae5daded0
Tresor1H <monseigneur140@gmail.com>
2026-05-16 00:55:18 +0000

    nouvelle modids

--- backend/chaincacao/settings.py
+++ backend/chaincacao/settings.py
@@ -70,7 +70,7 @@ if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
             default=DATABASE_URL,
-            conn_max_age=600,
+            conn_max_age=0, # Recommandé pour le pooling Supabase (Transaction mode)
             ssl_require=True
         )
     }
